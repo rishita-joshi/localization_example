a5f4b51a65fa3c9524febcdef01d25803bd0560a
rishita-joshi <joshirishita09@gmail.com>
2021-11-15 14:00:33 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 import 'package:provider/provider.dart';
 
 Future main() async {
